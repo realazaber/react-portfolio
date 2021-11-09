44c79d848cd6603452cc82f3887585c09d168bd9
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-10 04:39:12 +1100

    Updates

---
+++ precache-manifest.b9fde9a67cbaf2973883c0fc6a9b43ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ec7cea1e88e2e7a250a439f94f21cf",
+    "url": "/therealcoolpup/index.html"
+  },
+  {
+    "revision": "cf83198171c4447270fb",
+    "url": "/therealcoolpup/static/css/main.a5ac1d0e.chunk.css"
+  },
+  {
+    "revision": "0d57022965e587f61618",
+    "url": "/therealcoolpup/static/js/2.ac07ae4d.chunk.js"
+  },
+  {
+    "revision": "07ae7f89cd97805a01046175465ffdf3",
+    "url": "/therealcoolpup/static/js/2.ac07ae4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf83198171c4447270fb",
+    "url": "/therealcoolpup/static/js/main.380d1979.chunk.js"
+  },
+  {
+    "revision": "546db64958a572606be1",
+    "url": "/therealcoolpup/static/js/runtime-main.99292718.js"
+  },
+  {
+    "revision": "9e90889bcc3fd54a751932942c05eb4c",
+    "url": "/therealcoolpup/static/media/me.9e90889b.png"
+  }
+]);
